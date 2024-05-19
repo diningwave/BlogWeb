2f6a126ec236749496774f72bd1c6f428a28472b
diningwave-ceo <ceo@diningwave.com>
2024-05-19 22:10:28 +0900

    test

---
+++ src/main/java/com/ssamz/jblog/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.ssamz.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+	@GetMapping("/html")
+	public String html() {
+		System.out.println("HTML 파일이 요청됨 ") ; 
+		return "redirect:hello.html";
+	}
+	
+	@GetMapping("/image")
+	public String image() {
+		System.out.println("이미지 파일이 요청됨"); 
+		return "redirect:image/korean.png"; 
+	}
+	
+	@GetMapping("/jsp")
+	public String jsp(Model model) {
+		System.out.println("JSP 파일이 요청됨"); 
+		model.addAttribute("username", "쌤즈");
+		return "hello"; 
+	}
+
+}
